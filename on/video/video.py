0e3b1e620b6da978f9ef63a0fa9ac459befbd557
Toni Mueller <dev@tonimueller.org>
2011-12-14 02:53:38 +0100

    Added a basic view and renamed the type The runtime information is being tied to the view instead of the video object. May be open for debate.
    Testing is currently poor.

--- on/video/video.py
+++ on/video/video.py
@@ -36,12 +36,6 @@ class IVideo(form.Schema):
     filename = schema.TextLine(
         title=_(u"Basename of the video file"),
         )
-    
-    thumbnail = NamedImage(
-        title=_(u"Thumbnail"),
-        description=_(u"Here you can upload an image to display as a thumbnail."),
-        required=False,
-        )
 
     description = schema.Text(
         title=_(u"Teaser"),
@@ -62,6 +56,8 @@ class IVideo(form.Schema):
 
 
 import os.path
+#import csv
+import re
 from Acquisition import aq_inner
 from zope.component import queryUtility
 from plone.registry.interfaces import IRegistry
@@ -70,9 +66,9 @@ from on.video.configuration import IVideoConfiguration
 
 class vVideo(object):
 
-    def __init__(self, url, type):
+    def __init__(self, url, formatspec):
         self.url = url
-        self.type = type
+        self.format = formatspec
 
 
 class View(grok.View):
@@ -81,36 +77,85 @@ class View(grok.View):
 
     """Basic Video View"""
 
-    extensions = ('mp4', 'ogv', 'mpeg', 'avi', 'flv')
-
-    def videofiles(self):
-        """Return a list of video files"""
+    def videoMetaData(self, context):
+        """Read the video.metadata file to set some parameters. Helper function."""
+        print "setting up video meta data for ", context
         registry = queryUtility(IRegistry)
         settings = registry.forInterface(IVideoConfiguration)
-        context = aq_inner(self.context)
-        bpath = settings.fspath + '/' + context.filename + '.'
-        burl = settings.urlbase
-        
-        files = []
-        # need to do something to handle sizes, too:
-        for e in self.extensions:
-            fn = bpath + e
-            if os.path.exists(fn):
-                rfn = burl + context.filename + '.' + e
-                print "rfn: ", rfn
-                files.append(vVideo(rfn, e))
-        return files
+        if settings.urlbase[-1] != '/':
+            context.urlbase = context.urlbase + '/'
+        bpath = os.path.join(settings.fspath, context.filename, '.')
+        meta_path = bpath + "metadata"
+        if not os.path.exists(meta_path):
+            print "no metadata for ", context
+            context.thumbnailurl = None
+            context.playingtime = 'unknown'
+            context.urls = []
+            return None # can't raise an exception here
+        mdfile = open(meta_path, "rb")
+        # thumbnail file:
+        thumb = mdfile.next().split(':', 1)
+        context.thumbnailurl = None
+        if thumb[0].strip() == 'thumbnail' and len(thumb) > 1 and thumb[1].strip() != '':
+            context.thumbnailurl = settings.baseurl + thumb[1].strip()
 
+        # playing time:
+        ptime = mdfile.next().split(':', 1)
+        if ptime[0].strip() == 'playing time' and len(ptime) > 1:
+            pt = re.search('(\d+:\d\d:\d\d)', ptime[1])
+            if pt:
+                context.playingtime = pt.group()
+            else:
+                context.playingtime = 'unknown' #None #'0:00:00' # unnown playing time
+
+        # set url to the video that should be played inline (how to manage different sizes?):
+        svid = mdfile.next().split(':', 1)
+        if svid[0].strip() == 'selected':
+            vf = None
+            if len(svid) > 1:
+                vf = svid[1].strip()
+                if len(vf) > 1 and vf[0] == '/':
+                    vf = vf[1:]
+            if len(vf) and os.path.exists(os.path.join(settings.fspath, vf)):
+                context.directplay = settings.urlbase + vf
+        # now read the alternative formats list:
+        urls = []
+        for row in mdfile:
+            k, v = row.split(':', 1)
+            v = v.strip()
+            print "checking filenames: ", os.path.join(settings.fsbase, v)
+            if os.path.exists(os.path.join(settings.fsbase, v)):
+                files.append(vVideo("".join(settings.baseurl, v), k))
+        self.urls = urls
+
+    def videofiles(self):
+        """Return a list of video urls"""
+        context = aq_inner(self.context)
+        try:
+            x = context.urls
+        except:
+            self.videoMetaData(context)
+        #if not context.has_attr('urls'):
+        #    context.videoMetaData(context)
+        return context.urls
 
     def thumbnailurl(self):
         """Calculate the URL to the thumbnail"""
-        #if self.has_attr('thumbnail'):
-        registry = queryUtility(IRegistry)
-        settings = registry.forInterface(IVideoConfiguration)
         context = aq_inner(self.context)
-        for extension in ('png', 'jpeg', 'jpg', 'gif'):
-            maybe_thumbnail = settings.fspath + '/' + context.filename + '.' + extension
-            if os.path.exists(maybe_thumbnail):
-                return settings.urlbase + context.filename + '.' + extension
-        return '/++resource++on.video/nothumbnail.png'
-        #return settings.urlbase + '++resource++on.video/nothumbnail.png'
+        try:
+            x = context.urls
+        except:
+            self.videoMetaData(context)
+        if context.thumbnailurl is not None:
+            return context.thumbnailurl;
+        else:
+            return '/++resource++on.video/nothumbnail.png'
+
+    def playingtime(self):
+        """return the string for the playing time, if any"""
+        context = aq_inner(self.context)
+        try:
+            x = context.urls
+        except:
+            self.videoMetaData()
+        return context.playing_time

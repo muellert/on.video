102687dea703e24a00c9bc3ff26e24709ed2d96c
Toni Mueller <dev@tonimueller.org>
2013-07-16 01:28:19 +0200

    Attempt to improve the test suite. Especially, there are now more browser tests.
    One of them is disabled because I have to figure out why the
    registry change in the test suite's setUp() method does not
    work: The fspath is not being set to /var/tmp/something, but
    instead to /tmp, which causes the test to fail. If I step
    through the validator which is being tested (validateFilename()
    in the video.py module) and re-adjust the path to the temp
    directory which is being generated by the setUp() method, the
    test passes. :|

--- on/video/tests/test_functional.py
+++ on/video/tests/test_functional.py
@@ -37,7 +37,7 @@ class TestOnVideoHandling(unittest.TestCase):
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         registry = queryUtility(IRegistry)
-        self.settings = registry.forInterface(IVideoConfiguration)
+        settings = registry.forInterface(IVideoConfiguration)
         # generate a set of mockup files:
         self.td = tempfile.mkdtemp(prefix = "on.video-tests.")
         videofiles = ['sample_video_1_big.ogv', 'sample_video_1_medium.ogv', 'sample_video_1.mp4',
@@ -59,7 +59,9 @@ class TestOnVideoHandling(unittest.TestCase):
                 o = open(os.path.join(d, video), "wb")
                 o.write("1")
                 o.close()
-        self.settings.fspath = unicode(self.td)
+        settings.fspath = unicode(self.td)
+        #print "\n ====> TestOnVideoHandling: fspath = ", settings.fspath
+        self.settings = settings
 
 
     def tearDown(self):
@@ -78,7 +80,6 @@ class TestOnVideoHandling(unittest.TestCase):
         """Create a video object and inspect its attributes to see whether
            it conforms to the specs.
         """
-        #import pdb; pdb.set_trace()
         v = self.portal.invokeFactory('on.video.Video', 'video1', title=u"My Sample Video",
                                       name = 'some kind of video',
                                       author = 'me, myself',
@@ -152,7 +153,7 @@ class TestOnVideoHandling(unittest.TestCase):
         video = self.portal[v]
         import transaction; transaction.commit()
         view = video.restrictedTraverse('@@view')
-        import pdb; pdb.set_trace()
+        #import pdb; pdb.set_trace()
         self.failUnless(view.playing_time == 'unknown')
 
     def test_read_video_no_metadata(self):
@@ -161,29 +162,6 @@ class TestOnVideoHandling(unittest.TestCase):
            displays the appropriate placeholder image.
         """
         v = self.portal.invokeFactory('on.video.Video', 'video3', title=u"My Sample Video",
-                                      name = 'some kind of video',
-                                      author = 'me, myself',
-                                      recorded = datetime.now(),
-                                      filename = 'no-metadata',
-                                      place = 'nirvana',
-                                      body = RichTextValue('<strong>some interesting story</strong>'))
-        import transaction; transaction.commit()
-        app = self.layer['app']
-        browser = Browser(app)
-        browser.handleErrors = False
-        video = self.portal[v]
-        browser.open(video.absolute_url())
-        self.failUnless('nometafile' in browser.contents)
-
-    def XXtest_add_video_via_browser_no_metadata(self):
-        """Simulate adding a video object that has no metadata file.
-           The validator should raise an exception, which should
-           yield an error message in the browser.
-
-           We first need to learn about properly writing browser tests.
-        """
-        v = self.portal.invokeFactory('on.video.Video', 'video',
-                                      title=u"My Sample Unavailable Video",
                                       name = 'some kind of video',
                                       author = 'me, myself',
                                       recorded = datetime.now(),
@@ -240,26 +218,6 @@ class TestOnVideoHandling(unittest.TestCase):
         self.failUnless(view.playing_time == '20:30:50')
         self.failUnless(playlist[0].url.endswith(".mp4"))
         self.failUnless(downloads[0].url.endswith(".ogv"))
-
-    def test_video_summary_view(self):
-        """check whether the 'summary' view for the gallery works
-        """
-        v = self.portal.invokeFactory('on.video.Video', 'video4', title=u"My Sample Video",
-                                      name = 'some kind of video',
-                                      author = 'me, myself',
-                                      recorded = datetime.now(),
-                                      filename = 'sample_video_3',
-                                      place = 'nirvana',
-                                      body = RichTextValue('<strong>some interesting story</strong>'))
-        video = self.portal[v]
-        import transaction; transaction.commit()
-        app = self.layer['app']
-        browser = Browser(app)
-        browser.handleErrors = False
-        video = self.portal[v]
-        browser.open(video.absolute_url() + '/@@summary')
-        self.failUnless('<div class="on-video-small-thumbnail">' in browser.contents)
-        self.failIf('<object>' in browser.contents)
 
     def test_video_summary_view(self):
         """check whether the 'summary' view for the gallery works
@@ -356,23 +314,6 @@ class TestOnVideoHandling(unittest.TestCase):
         self.failUnless(view.x == DEFAULT_WIDTH)
         self.failUnless(view.y == DEFAULT_HEIGHT)
 
-    def test_read_video_with_no_playing_time(self):
-        """See whether the file is correctly read if the playing time
-           is not specified.
-        """
-        from on.video.config import DEFAULT_WIDTH, DEFAULT_HEIGHT
-        v = self.portal.invokeFactory('on.video.Video', 'video8', title=u"My Sample Video",
-                                      name = 'some kind of video',
-                                      author = 'me, myself',
-                                      recorded = datetime.now(),
-                                      filename = 'sample_video_6',
-                                      place = 'nirvana',
-                                      body = RichTextValue('<strong>some interesting story</strong>'))
-        video = self.portal[v]
-        import transaction; transaction.commit()
-        view = video.restrictedTraverse('@@view')
-        self.failUnless(view.playing_time == 'unknown')
-
     def test_fix_settings_url(self):
         """See whether the URL is properly fixed.
         """
@@ -405,8 +346,52 @@ class TestOnVideoHandling(unittest.TestCase):
         vo = parseMetadataFileContents(lines, settings.urlbase, settings.fspath, 'corrupted1')
         self.failUnless(vo.thumbnailurl == '/++resource++on.video/invalidmetafile.png')
 
+    def test_add_video_via_browser_no_metadata(self):
+        """Simulate adding a video object that has no metadata file.
+           The validator should raise an exception, which should
+           yield an error message in the browser.
+
+           We first need to learn about properly writing browser tests.
+        """
+        v = self.portal.invokeFactory('on.video.Video', 'video',
+                                      title=u"My Sample Unavailable Video",
+                                      name = 'some kind of video',
+                                      author = 'me, myself',
+                                      recorded = datetime.now(),
+                                      filename = 'no-metadata',
+                                      place = 'nirvana',
+                                      body = RichTextValue('<strong>some interesting story</strong>'))
+        import transaction; transaction.commit()
+        app = self.layer['app']
+        browser = Browser(app)
+        browser.handleErrors = False
+        video = self.portal[v]
+        browser.open(video.absolute_url())
+        self.failUnless('nometafile' in browser.contents)
+
+    # below: test for the three required fields
+    # title, filename, and body
 
     def test_add_video_without_title(self):
+        """Fill out the video 'add' form without giving a title, so
+           that it does not validate.
+        """
+        app = self.layer['app']
+        browser = Browser(app)
+        #browser.handleErrors = False
+        # get the relevant fields:
+        browser.addHeader('Authorization', 'Basic %s:%s' % (
+            SITE_OWNER_NAME, SITE_OWNER_PASSWORD))
+        browser.open(self.portal.absolute_url() + '/++add++on.video.Video')
+        filename_field = browser.getControl(name='form.widgets.filename')
+        body_field = browser.getControl(name='form.widgets.body')
+        save_button = browser.getControl(name='form.buttons.save')
+        filename_field.value = u'sample_video_3'
+        body_field.value = u'<p>nothing to see here, move along</p>'
+        save_button.click()
+        self.failUnless("Required input is missing." in browser.contents)
+
+    def test_add_video_without_metadatafile(self):
         """Fill out the video 'add' form with the wrong date and notice
            that it does not validate.
         """
@@ -418,18 +403,64 @@ class TestOnVideoHandling(unittest.TestCase):
             SITE_OWNER_NAME, SITE_OWNER_PASSWORD))
         browser.open(self.portal.absolute_url() + '/++add++on.video.Video')
         title_field = browser.getControl(name='form.widgets.title')
-        recorded_field_year = browser.getControl(name='form.widgets.recorded-year')
-        recorded_field_month = browser.getControl(name='form.widgets.recorded-month')
-        recorded_field_day = browser.getControl(name='form.widgets.recorded-day')
-        recorded_field_hour = browser.getControl(name='form.widgets.recorded-hour')
-        recorded_field_min = browser.getControl(name='form.widgets.recorded-min')
+        body_field = browser.getControl(name='form.widgets.body')
+        save_button = browser.getControl(name='form.buttons.save')
+        title_field.value = u'Hi there!'
+        body_field.value = u'<p>nothing to see here, move along</p>'
+        save_button.click()
+        self.failUnless("Required input is missing." in browser.contents)
+
+    def test_add_video_without_body(self):
+        """Fill out the video 'add' form with the wrong date and notice
+           that it does not validate.
+        """
+        app = self.layer['app']
+        browser = Browser(app)
+        #browser.handleErrors = False
+        # get the relevant fields:
+        browser.addHeader('Authorization', 'Basic %s:%s' % (
+            SITE_OWNER_NAME, SITE_OWNER_PASSWORD))
+        browser.open(self.portal.absolute_url() + '/++add++on.video.Video')
+        title_field = browser.getControl(name='form.widgets.title')
+        filename_field = browser.getControl(name='form.widgets.filename')
+        save_button = browser.getControl(name='form.buttons.save')
+        title_field.value = u'Hi there!'
+        filename_field.value = u'sample_video_3'
+        save_button.click()
+        self.failUnless("Required input is missing." in browser.contents)
+
+
+    ### Disable this test for now until we figure out why the fspath registry
+    ### entry is not being properly set up by the setUp() method (it returns
+    ### u'/tmp' in all cases, instead of the generated temp dir, causing this
+    ### test to fail):
+    def XXXtest_add_video_with_missing_thumbnail_but_thumbnail_line_in_metadata(self):
+        """Now try sample_metadata_1 to verify that the thumbnail is
+           being handled correctly.
+        """
+        app = self.layer['app']
+        browser = Browser(app)
+        #browser.handleErrors = False
+        # get the relevant fields:
+        browser.addHeader('Authorization', 'Basic %s:%s' % (
+            SITE_OWNER_NAME, SITE_OWNER_PASSWORD))
+        browser.open(self.portal.absolute_url() + '/++add++on.video.Video')
+        title_field = browser.getControl(name='form.widgets.title')
         filename_field = browser.getControl(name='form.widgets.filename')
         body_field = browser.getControl(name='form.widgets.body')
         save_button = browser.getControl(name='form.buttons.save')
-        # fill the fields and save the form:
-        filename_field.value = u'sample_video_3'
+        title_field.value = u'Hi there!'
+        filename_field.value = u'sample_video_1'
         body_field.value = u'<p>nothing to see here, move along</p>'
         save_button.click()
-        #import pdb; pdb.set_trace()
-        vl = self.portal.keys()
-        #self.failUnless(True)
+        import pdb; pdb.set_trace()
+        self.failUnless("nothumbnail" in browser.contents)
+
+
+# need to check for bogus dates someday, too:
+
+#        recorded_field_year = browser.getControl(name='form.widgets.recorded-year')
+#        recorded_field_month = browser.getControl(name='form.widgets.recorded-month')
+#        recorded_field_day = browser.getControl(name='form.widgets.recorded-day')
+#        recorded_field_hour = browser.getControl(name='form.widgets.recorded-hour')
+#        recorded_field_min = browser.getControl(name='form.widgets.recorded-min')
